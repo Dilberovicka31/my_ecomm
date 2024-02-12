35949d75989805a91dca4e41f1a01c82e46f341f
Mija Dilberovic <mijadilberovic@Mijas-MacBook-Pro.local>
2024-02-12 11:23:16 -0800

    adjusted connection file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,6 @@
-// server/config/db.js
+require("dotenv").config();
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost/ecommerce_web', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit the process with an error code
-  }
-};
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/my_ecomm');
 
-module.exports = connectDB;
+module.exports = mongoose.connection;
