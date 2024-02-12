456384498c9263a78261a732915b0e211e3b24ae
Mija Dilberovic <mijadilberovic@Mijas-MacBook-Pro.local>
2024-02-12 10:21:28 -0800

    adjusted resolvers.js

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,3 +1,34 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-const resolvers= {}
+// Hash the user's password before saving it to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password') || user.isNew) {
+    try {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      user.password = hashedPassword;
+      next();
+    } catch (error) {
+      return next(error);
+    }
+  } else {
+    return next();
+  }
+});
+
+// Compare user-entered password with the hashed password in the database
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
